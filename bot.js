ad71516b60b528bd44ffcde229427aed06a52c9e
System <none>
2017-02-05 05:33:45 +0000

    🌈👕 Updated with Gomix

--- bot.js
+++ bot.js
@@ -8,6 +8,8 @@ const request = require('request');
 const path = require('path');
 const categories = require('./categories');
 const events = require('./events');
+
+var EventSearch = require("facebook-events-by-location-core");
 var Wit = require('node-wit').Wit;
 var messengerButton = "<html><head><title>Facebook Messenger Bot</title></head><body><h3>QHacks 2017 Facebook Messenger Bot Example</h3>This is a bot based on Messenger Platform QuickStart. Find more details <a href=\"https://developers.facebook.com/docs/messenger-platform/guides/quick-start\">here</a><br><hr><p><a href=\"https://gomix.com/#!/remix/messenger-bot/ca73ace5-3fff-4b8f-81c5-c64452145271\"><img src=\"https://gomix.com/images/background-light/remix-on-gomix.svg\"></a></p><p><a href=\"https://gomix.com/#!/project/messenger-bot\">View Code</a></p></body></html>";
 
@@ -82,11 +84,15 @@ function receivedMessage(event) {
   console.log("Received message for user %d and page %d at %d with message:", 
     senderID, recipientID, timeOfMessage);
   console.log(JSON.stringify(message));
+  console.log(JSON.stringify(message.attachments));
 
   var messageId = message.mid;
 
   var messageText = message.text;
   var messageAttachments = message.attachments;
+  
+  // var lat = messageAttachments[0].payload.coordinates.lat;
+  // var lng = messageAttachments[0].payload.coordinates.long;
 
   if (messageText) {
     
@@ -96,9 +102,11 @@ function receivedMessage(event) {
       case 'generic':
         sendGenericMessage(senderID);
         break;
+        /*
       case 'hello':
         sendTextMessage(senderID, 'Hello yourself!');
         break;
+        */
       default:
         //sendTextMessage(senderID, messageText);
         
@@ -109,6 +117,7 @@ function receivedMessage(event) {
         wit.runActions(
               sessionId, // the user's current session
               messageText, // the user's message
+              //messageAttachments, // the users data
               sessions[sessionId].context // the user's current session state
             ).then((context) => {
               // Our bot did everything it has to do.
@@ -130,7 +139,12 @@ function receivedMessage(event) {
             })
     }
   } else if (messageAttachments) {
-    sendTextMessage(senderID, "Message with attachment received");
+    if (messageAttachments[0].type === "location"){
+      let {lat, long} = messageAttachments[0].payload.coordinates
+      getLocalEvents(senderID, lat, long);
+    } else {
+      sendTextMessage(senderID, "Message with attachment received");
+    }
   }
 }
 
@@ -179,8 +193,9 @@ const findOrCreateSession = (fbid) => {
 
 // Our bot actions
 const actions = {
-  send({sessionId}, {text, quickreplies}) {
-
+  send({sessionId, messageAttachments}, {text, quickreplies}) {
+    
+    console.log(messageAttachments);
     // Our bot has something to say!
     // Let's retrieve the Facebook user whose session belongs to
     const recipientId = sessions[sessionId].fbid;
@@ -207,7 +222,7 @@ const actions = {
 
   // You should implement your custom actions here
   // See https://wit.ai/docs/quickstart
-  findEvent({context, entities}) {
+  findAttributeByEvent({sessionId, context, entities}) {
     return new Promise(function(resolve, reject) {
       // Here should go the api call, e.g.:
       // context.forecast = apiCall(context.loc)
@@ -219,7 +234,13 @@ const actions = {
         case 'findEvent':
           console.log("Executing findEvent");
           
-          context.event = selectEvent(entities);
+          let selectedEvent = selectEvent(entities)
+          
+          sendEventGenericMessage(sessions[sessionId].fbid, [selectedEvent]);
+          
+          context.eventTitle = selectedEvent.title;
+          break;
+        case 'findEventCategory':
           break;
         case 'findEventTime':
           console.log("Executing findEventTime");
@@ -231,6 +252,19 @@ const actions = {
           context.startTime = dateToReadableString(result.startTime, options);
           context.endTime = dateToReadableString(result.endTime, options);
           break;
+        case 'findEventLocation':
+          console.log("Executing findEventLocation");
+          console.log(entities);
+          
+          let selectedEvent2 = selectEvent(entities);
+          console.log(context)
+          console.log(selectedEvent2)
+          
+          context.eventTitle = selectedEvent2.title;
+          context.locationPlace = selectedEvent2.location.place;
+          context.locationCity = selectedEvent2.location.city;
+          
+          break;
         default:
           console.log("No")
       }
@@ -239,12 +273,17 @@ const actions = {
     });
   },
   
-  getEvents({context, entities}){
+  getEvents({sessionId, context, entities}){
     return new Promise(function(resolve, reject) {
       //console.log("we exist");
       //console.log(events.slice(0,3))
-      var rand_id = Math.floor(Math.random() * (events.length));
-      context.events = events[rand_id];
+      //var rand_id = Math.floor(Math.random() * (events.length));
+      context.events = events.slice(0,5);//[rand_id];
+      
+      console.log(sessions[sessionId]);
+      
+      sendEventGenericMessage(sessions[sessionId].fbid, context.events);
+      
       return resolve(context);
     });
   },
@@ -252,129 +291,77 @@ const actions = {
   getEventCategories({context, entities}){
     return new Promise(function(resolve, reject) {
       const categories_array = [];
+      let categories_str = "";
+      
       for (var key in categories) {
         if (categories.hasOwnProperty(key)) {
           categories_array.push(categories[key]);
         }
       }
-      context.categories = categories_array;
-      return resolve(context);
-    });
-  },
-  
-  findEventsByAttribute({context, entities}){
-    return new Promise(function(resolve, reject) {
-      // Here should go the api call, e.g.:
-      // context.forecast = apiCall(context.loc)
-      //console.log(entities.category[0].value)
-      //console.log(dateToReadableString(entities.datetime[0].value));
       
-      console.log(entities)
-      
-      
-      var selectedEvents = events.filter(function(event) { 
-      // if (entities.datetime) {
-      //   return event.category === entities.category[0].value && event.datetime === entities.datetime[0].value;
-      // } else {
-        return event.category === entities.category[0].value;
-      //}
-      });
-
-      if (selectedEvents.length === 0) {
-        context.missingEvents = 'missing';
-      } else {
-        context.events = selectedEvents;
-        //context.startTime = selectedEvents.startTime;
-      }
-
-      console.log(context.missingEvents);
-      
-      return resolve(context);
-    });
-  },
-  
-  findEventsByCategory({context, entities}){
-    return new Promise(function(resolve, reject) {
-      // Here should go the api call, e.g.:
-      // context.forecast = apiCall(context.loc)
-      //console.log(entities.category[0].value)
-      //console.log(dateToReadableString(entities.datetime[0].value));
-      var selectedEvents = events.filter(function(event) { 
-      // if (entities.datetime) {
-      //   return event.category === entities.category[0].value && event.datetime === entities.datetime[0].value;
-      // } else {
-        return event.category === entities.category[0].value;
-      //}
-      });
-
-      if (selectedEvents.length === 0) {
-        context.missingEvents = 'missing';
-      } else {
-        context.events = selectedEvents;
-        //context.startTime = selectedEvents.startTime;
-      }
-
-      console.log(context.missingEvents);
-      
-      return resolve(context);
-    });
-  },
-  
-  findEventsByCategoryandDatetime({context, entities}){
-    return new Promise(function(resolve, reject) {
-      // Here should go the api call, e.g.:
-      // context.forecast = apiCall(context.loc)
-      //console.log(entities.category[0].value)
-      //console.log(dateToReadableString(entities.datetime[0].value));
-      var selectedEvents = events.filter(function(event) { 
-        if (entities.datetime) {
-          return event.category === entities.category[0].value && event.datetime === entities.datetime[0].value;
-        } else {
-          return event.category === entities.category[0].value;
-          }
-      });
-
-      if (selectedEvents.length === 0) {
-        context.missingEvents = 'missing';
-      } else {
-        context.events = selectedEvents;
-        context.startTime = selectedEvents.startTime;
-      }
-
-      console.log(context.missingEvents);
-      
-      return resolve(context);
-    });
-  },
-  
-  findEventsByCategoryandLocation({context, entities}){
-    return new Promise(function(resolve, reject) {
-      // Here should go the api call, e.g.:
-      // context.forecast = apiCall(context.loc)
-      console.log(entities.category[0].value)
-      //console.log(dateToReadableString(entities.datetime[0].value));
-      var selectedEvents = events.filter(function(event) { 
+      for (let key in categories) {
         
-        // location is not working currently
-        //if (entities.location) {
-        //  return event.category === entities.category[0].value && event.location === entities.location[0].value;
-        //} else {
-          return event.category === entities.category[0].value;
-        //}
-      });
+        if (categories[key] === categories_array[categories_array.length - 2])
+          categories_str += categories[key] + ", and ";
+        else if (categories[key] !== categories_array[categories_array.length - 1])
+          categories_str += categories[key] + ", ";
+        else
+          categories_str += categories[key] + ". "
+      }
       
-      console.log(selectedEvents)
+      context.categoriesStr = categories_str;
+     
+      return resolve(context);
+    });
+  },
+  
+  findEventsByAttribute({sessionId, context, entities}){
+    return new Promise(function(resolve, reject) {
+      // Here should go the api call, e.g.:
+      // context.forecast = apiCall(context.loc)
+      //console.log(entities.category[0].value)
+      //console.log(dateToReadableString(entities.datetime[0].value));
+      
+      console.log("Time")
+      console.log(entities.datetime[0])
+      
+      var selectedEvents = events.filter(function(event) {
+        
+        // only consider checking whether this event should be returned
+        // only if the user asked for events with these attributes
+        // ie. these attributes are in entities
+        if (entities.category && entities.datetime) {
+          return event.category === entities.category[0].value &&  sameDate(event.startTime, entities.datetime[0].value);
+        }
+        else if (entities.category) {
+          return event.category === entities.category[0].value;
+        }
+        else if (entities.datetime) {
+          console.log("datetime")
+          console.log(entities.datetime[0].values)
+          if (typeof entities.datetime[0].to !== 'undefined') {
+              return sameDate(event.startTime, entities.datetime[0].value) || withinDateRange(event.startTime, entities.datetime[0].to, entities.datetime[0].from);
+          }
+          return sameDate(event.startTime, entities.datetime[0].value);
+        }
+        
+      });
 
       if (selectedEvents.length === 0) {
         context.missingEvents = 'missing';
       } else {
         context.events = selectedEvents;
-        context.startTime = selectedEvents.startTime;
+        context.eventDay = entities.datetime[0].value.toLocaleString('en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'});
       }
-
-      console.log(context.missingEvents);
+      if (context.events) { sendEventGenericMessage(sessions[sessionId].fbid, context.events); }
       
       return resolve(context);
+    });
+  },
+  
+  findLocalEvents({sessionId, context, entities}) {
+    return new Promise(function(resolve, reject) {
+      sendLocationPrompt(sessions[sessionId].fbid);
     });
   },
 };
@@ -427,13 +414,56 @@ function findByCategory(context, entities) {
     return context;
 }
 
-function dateToReadableString(timestamp, formattingOptions={ weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' }) {
+// return true if two datetimes are on the same day
+// check the exact time if sameTimeToo is true
+function sameDate(dateTime1, dateTime2, sameTimeToo=false) {
+  
+  dateTime1 = new Date(Date.parse(dateTime1))
+  dateTime2 = new Date(Date.parse(dateTime2))
+  
+  // check if the datetimes are on the same day if sameTimeToo is false
+  if (!sameTimeToo) {
+    dateTime1 = dateTime1.toDateString()
+    dateTime2 = dateTime2.toDateString()
+  }
+  
+  return dateTime1 === dateTime2;
+}
+
+function withinDateRange(dateTime1, dateTimeRange1, dateTimeRange2) {
+  dateTime1 = new Date(Date.parse(dateTime1))
+  dateTimeRange1 = new Date(Date.parse(dateTimeRange1))
+  dateTimeRange2 = new Date(Date.parse(dateTimeRange2))
+  
+  return dateTime1 >= dateTimeRange1 && dateTime1 <= dateTimeRange1;
+}
+
+function dateToReadableString(timestamp) {
   var result = new Date(Date.parse(timestamp));
+  let formattingOptions = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'}
   
-  console.log(result.getMonth());
-  console.log(result.getUTCDay());
-  
-  return result.toLocaleString('en-US', formattingOptions);
+  return result.toLocaleString('en-US', formattingOptions) + " at " + result.toLocaleTimeString({hour: 'numeric', minute: '2-digit', timeZoneName: 'short'});
+}
+
+function getLocalEvents(recipientId, lat, lng){
+  var es = new EventSearch({
+    "lat": lat,
+    "lng": lng,
+    "distance": 1000, // 1 km
+    "sort": "popularity"
+  });
+
+  es.search().then(function (events) {
+    var events_limit;
+    if(events.events.length > 10){
+      events_limit = events.events.slice(0,10);
+    } else {
+      events_limit = events.events;
+    }
+    sendLocalEventGenericMessage(recipientId, events_limit);
+  }).catch(function (error) {
+      console.error(JSON.stringify(error));
+  });
 }
 
 
@@ -461,6 +491,112 @@ function sendTextMessage(recipientId, messageText, quickReplies) {
     }
   };
 
+  callSendAPI(messageData);
+}
+
+// send location prompt to gain local data from user
+function sendLocationPrompt(recipientId) {
+
+  var messageData = {
+    "recipient":{
+      "id": recipientId
+    },
+    "message":{
+      "text":"Please share your location:",
+      "quick_replies":[
+        {
+          "content_type":"location",
+        }
+      ]
+    }
+  }
+  callSendAPI(messageData);
+}
+
+// send event data following the generic template
+function sendLocalEventGenericMessage(recipientId, eventObjectList) {
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      attachment: {
+        type: "template",
+        payload: {
+          template_type: "generic",
+          elements: []
+        }
+      }
+    }
+  };
+
+  for (let e in eventObjectList) {
+    
+    let dateTime = new Date(Date.parse(eventObjectList[e].startTime))
+
+    let item_url = 'https://facebook.com/events/' + eventObjectList[e].id; 
+    
+    messageData.message.attachment.payload.elements.push(
+      {
+        title: eventObjectList[e].name,
+        subtitle: dateToReadableString(dateTime) + eventObjectList[e].venue.location, 
+        item_url: item_url,
+        image_url: eventObjectList[e].coverPicture,
+        buttons: [{
+          type: 'web_url',
+          url: item_url,
+          title: 'Learn More',
+        },
+        {
+          type: "element_share"    
+        }
+        ],
+      }
+    );
+  }
+  
+  callSendAPI(messageData);
+}
+
+function sendEventGenericMessage(recipientId, eventObjectList) {
+ var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      attachment: {
+        type: "template",
+        payload: {
+          template_type: "generic",
+          elements: []
+        }
+      }
+    }
+  };
+
+  for (let e in eventObjectList) {
+    
+    let dateTime = new Date(Date.parse(eventObjectList[e].startTime))
+    
+    messageData.message.attachment.payload.elements.push(
+      {
+        title: eventObjectList[e].title,
+        subtitle: dateToReadableString(dateTime) + "\n Kingston, Ontario", // eventObjectList[e].location,
+        item_url: eventObjectList[e].item_url,
+        image_url: eventObjectList[e].image_url,
+        buttons: [{
+          type: 'web_url',
+          url: eventObjectList[e].item_url,
+          title: 'Learn More',
+        },
+        {
+          type: "element_share"    
+        }
+        ],
+      }
+    );
+  }
+  
   callSendAPI(messageData);
 }
 
--- categories.js
+++ categories.js
@@ -1,6 +1,6 @@
 const categories = {
-  CONCERTS: 'concerts', MOVIES: 'movies', ADULT_SOCIALS:'19+socials', ALL_AGES_SOCIALS:'allagessocials', 
-  ARTS_AND_THEATER:'arts&theatre', EDUCATIONAL:'educational', HEALTH:'health', SPORTS: 'sports'
+  CONCERTS: 'concerts', MOVIES: 'movies', ADULT_SOCIALS:'19+ socials', ALL_AGES_SOCIALS:'all ages socials', 
+  ARTS_AND_THEATER:'arts & theatre events', EDUCATIONAL:'educational events', HEALTH:'health & wellness events', SPORTS: 'sport events'
 };
 
 module.exports = categories;
